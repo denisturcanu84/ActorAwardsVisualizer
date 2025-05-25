3e0a7af2d2524f9d75812d5de2245a8c73ac16df
Meraru Ioan-Lucian <ioan.lucian.meraru@gmail.com>
2025-05-25 18:53:23 +0300

    d

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar">
     <span class="logo">ActorAwardsVisualizer</span>
     <div class="nav-links">
-        <li><a href="searchActor.php" class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'searchActor.php' ? 'active' : ''; ?>">Home</a></li>
+        <a href="../pages/startpage.php">Home</a>
         <a href="404.html">Statistics</a>
         <a href="404.html">About</a>
     </div>
