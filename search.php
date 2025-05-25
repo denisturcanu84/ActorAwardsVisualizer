bf8b3bda9f78c6130cca1d00ed4fad3afacb5a5c
Meraru Ioan-Lucian <ioan.lucian.meraru@gmail.com>
2025-05-25 17:32:06 +0300

    3rg3g34g

--- search.php
+++ search.php
@@ -12,7 +12,7 @@ if ($search !== '') {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Actor Awards Visualizer - Search wgbqi3bgfijlqwbgiljweqbiflqwb</title>
+    <title>Actor Awards Visualizer - Search wgbqi3bgfijlqwbgiljweqbiflqw</title>
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/index.css">
     <link rel="stylesheet" href="assets/css/navbar.css">
@@ -20,7 +20,7 @@ if ($search !== '') {
 <body>
 <?php include 'includes/navbar.php'; ?>
     <div class="container">
-        <h1>Search Actor Profile</h1>
+        <h1>Search Actor Profile lalalalala</h1>
         <form method="get" action="index.php">
             <input type="text" name="name" placeholder="Enter actor's full name" required>
             <br>
