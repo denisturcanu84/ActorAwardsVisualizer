ddc0999f3e37b46c242750288f10a131fc15304b
Meraru Ioan-Lucian <ioan.lucian.meraru@gmail.com>
2025-05-25 19:56:22 +0300

    navbar mobile

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,7 @@
 <nav class="navbar">
     <span class="logo">ActorAwardsVisualizer</span>
+    <input type="checkbox" id="menu-toggle" class="menu-toggle">
+    <label for="menu-toggle" class="menu-button">☰</label>
     <div class="nav-links">
         <a href="../pages/startpage.php">Home</a>
         <a href="404.html">Statistics</a>
