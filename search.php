da400c163869174076c589d79e05194866a47c20
Meraru Ioan-Lucian <ioan.lucian.meraru@gmail.com>
2025-05-25 17:30:23 +0300

    comitul iar

--- search.php
+++ search.php
@@ -12,7 +12,7 @@ if ($search !== '') {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Actor Awards Visualizer - Search</title>
+    <title>Actor Awards Visualizer - Search wgbqi3bgfijlqwbgiljweqbiflqwb</title>
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/index.css">
     <link rel="stylesheet" href="assets/css/navbar.css">
