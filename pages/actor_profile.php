c7ee5105180e3dcb950dbb70333aa504519adb84
Meraru Ioan-Lucian <ioan.lucian.meraru@gmail.com>
2025-05-25 18:47:23 +0300

    f

--- pages/actor_profile.php
+++ pages/actor_profile.php
@@ -1,8 +1,8 @@
 <?php
-require_once __DIR__ . '../includes/db.php';
-require_once __DIR__ . '../includes/tmdb.php';
-require_once __DIR__ . '../includes/functions.php';
-require_once __DIR__ . '../vendor/autoload.php';
+require_once __DIR__ . '/../includes/db.php';
+require_once __DIR__ . '/../includes/tmdb.php';
+require_once __DIR__ . '/../includes/functions.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
